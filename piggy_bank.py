70e33a08ad088319cd7f25a3263d86c1bc40a4f8
ravinarra <38094468+ravinarra@users.noreply.github.com>
2019-04-16 00:20:30 -0700

    Add files via upload

---
+++ piggy_bank.py
@@ -0,0 +1,35 @@
+#Piggy Bank Software
+
+class bank():
+    def __init__(self):
+        self.balance=0
+        
+    def add(self):
+        amount = float(input("\nAdd amount: "))
+        self.balance += amount
+        print("\nAfter adding , your updated balance is {} rupees".format(self.balance))
+        print("None\n")
+        
+    def withdraw(self):
+        amount = float(input("\nwithdrawing amount: "))
+        self.balance -= amount
+        print("\nAfter withdrawing , updated balance is {} rupees".format(self.balance))
+        print("None\n")
+        
+    def check(self):
+        print("\nYour current balance is {} rupees".format(self.balance))
+        print("None\n")
+    
+    def start_application(self):
+        app=input("Start or End:")
+        while app == 'Start':
+            act=input("Add, Withdraw or Check:")
+            if act=='Add':
+                self.add()
+            elif act=='Withdraw':
+                self.withdraw()
+            elif act=='Check':
+                self.check()
+            else:
+                print("Please enter correct commend!")
+            app=input("Start or End:") 
